74a3492ded33f4ea7c94c4432828729f778ea73e
simone tardiani <simone.tardiani@gmail.com>
2022-02-08 16:48:13 +0100

    bunch of stuff

---
+++ VS/vs/C/Es_Informatica_2021_Marco/220204_struct2_tardiani_simone.c
@@ -0,0 +1,19 @@
+//
+
+#include<stdio.h>
+#include<string.h>
+
+struct Prodotto{
+    char descrizione[80];
+    int quantita;
+    int costo;
+};
+
+typedef struct Prodotto Item;
+
+int main(){
+    
+    
+
+    return 0;
+}
--- VS/vs/C/TEST/TEST.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    printf("%d\n",5/2);
-    return 0;
-}
