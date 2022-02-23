744e244e8620c7d62813d1cabd0650e76a166e5a
Simone Tardiani <Simone.Tardiani@gmail.com>
2022-02-23 12:23:59 +0100

    Changed name of some dirs and added a todo for me

---
+++ Todo.md
@@ -0,0 +1,3 @@
+# My TODO list
+
+- [ ] Olimpiadi cybersecurity in 4a
---
+++ VS/vs/C/Es_Informatica_2021_Marco/220223_ricorsioneNpari_tardiani_simone.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int Npari(int a,int i,int sum);
+
+int main(){
+    
+    int a=0;
+
+    printf("Numero:");
+    scanf("%d",&a);
+
+    printf("%d\n",Npari(a,0,0));
+
+    return 0;
+}
+
+int Npari(int a,int i,int sum){
+
+    i++;
+
+    if ( i%2==0)
+    {
+        a--;
+        sum+=i;
+    }
+    
+    if(a==0){
+        return sum;
+    }else
+    {
+        return Npari(a,i,sum);
+    }
+}
