1b75abd686eca97b8df129bdc478ee2194ab2722
Simone Tardiani <Simone.Tardiani@gmail.com>
2022-02-25 11:09:49 +0100

    c exercises added

--- VS/vs/C/Es_Informatica_2021_Marco/220223_ricorsioneNpari_tardiani_simone.c
+++ VS/vs/C/Es_Informatica_2021_Marco/220223_ricorsioneNpari_tardiani_simone.c
@@ -1,28 +1,28 @@
 #include<stdio.h>
 
-int Npari(int a,int i,int sum);
+int Npari(int a);
 
 int main(){
     int a=0;
 
     printf("Numero:");
     scanf("%d",&a);
-    printf("%d\n",Npari(a,0,0));
+    printf("%d\n",Npari(a));
 
     return 0;
 }
 
-int Npari(int a,int i,int sum){
-    i++;
-
-    if ( i%2==0){
-        a--;
-        sum+=i;
-    }
-
+int Npari(int a){
     if(a==0){
-        return sum;
+        return a;
     }else{
-        return Npari(a,i,sum);
+        return a*2+Npari(a-1);
+    }
+}
+int Npari(int a){
+    if(a==0){
+        return a;
+    }else{
+        return a*2+Npari(a-1);
     }
 }
