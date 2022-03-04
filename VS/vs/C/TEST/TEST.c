381520fcd8a6f80fb82f5c693dcabe1409d88c75
Simone Tardiani <Simone.Tardiani@gmail.com>
2022-03-04 11:48:10 +0100

    .

--- VS/vs/C/TEST/TEST.c
+++ VS/vs/C/TEST/TEST.c
@@ -18,15 +18,9 @@ int main(){
 */
 
 int main(){
-    
-    char *ar[80];
 
-    printf("a:");
-    gets(ar);
-
-    ar = (char *) malloc((strlen(ar)+1) * sizeof(char));
-
-    puts(a);
-    printf("%d\n",sizeof(ar));
+    int *p_v, v[10]={0,1,2,3,4,5,6,7,8,9};
+    p_v=v;
+    printf("%d\n",*p_v);
     return 0;
 }
