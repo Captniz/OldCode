851c15aa1232e82512a10c0645094dc3430c0a7b
Simone Tardiani <Simone.Tardiani@gmail.com>
2022-03-09 09:39:57 +0100

    Removed EXEs and added 2 c exercises

---
+++ VS/vs/C/Es_Informatica_2021_Marco/220309_PushAndPop_tardiani_simone.c
@@ -0,0 +1,34 @@
+/*
+Con le primitive:
+Push
+Pop
+IsEmpty
+IsFull
+Top
+
+caricare visualizzare e ricercare in un array dim 10 rispettando la struttura 
+adt dello stack.
+Push e pop tologono da sopra.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#define DIM 10
+
+Push(int v,int n);
+Pop(int v);
+
+int main(){
+    int Stack[DIM];
+
+    return 0;
+}
+
+Push(int v,int n){
+    int i=0;
+
+    for ( i = 0; i < DIM-1; i++){
+        v[i]
+    }
+    
+}
+Pop(int v);
