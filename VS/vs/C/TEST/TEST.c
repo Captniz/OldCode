a02d077c01db61f49d8b73bd27a4699716bfd61a
simone tardiani <simone.tardiani@gmail.com>
2022-03-15 21:31:46 +0100

    .

--- VS/vs/C/TEST/TEST.c
+++ VS/vs/C/TEST/TEST.c
@@ -1,26 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-/*
+
+//! Indirizzo Puntatore => Puntatore => Indirizzo => Valore
+//! & = Indirizzo
+//! * = Valore
+
+int funz (int *a, int b);
+
 int main(){
+    int a,b,v[10]={0,1,2,3,4,5,6,7,8,9};
+    a=0;
+    b=0;
     
-    char a[80], *ar;
+    int *p_b;
+    p_b = v;
 
-    printf("a:");
-    gets(a);
+    // &a = 45678456788
+    funz(&a,b);
+    printf("%d %d\n",a,b);
+    printf("%d %d",*v,*p_b);
+}
 
-    ar = (char *) malloc((strlen(a)+1) * sizeof(char));
+int funz(int *c, int d){
+    //! Indirizzo Puntatore => Puntatore => Indirizzo => Valore
+    //! & = Indirizzo
+    //! * = Valore
 
-    puts(a);
+    // c => &a = 45678456788 
+    // *c => *(&a) => a 
+
+    *c=*c+100;
+    d++;
 
     return 0;
-}
-*/
-
-int main(){
-    int v[10];
-
-    scanf("%d",v[0]);
-
-    printf("%d\n",v[0]);
 }
