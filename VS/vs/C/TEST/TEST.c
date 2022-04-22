6810e7dce4d23d4feb0039f55224d5aac568a79f
simone tardiani <simone.tardiani@gmail.com>
2022-04-22 23:09:03 +0200

    .

--- VS/vs/C/TEST/TEST.c
+++ VS/vs/C/TEST/TEST.c
@@ -1,13 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#define DIM 3
+#define DIM 4
+/*TODO :
+idea -> 
+//* mettere la stringa sotto esame e il buffer in una struct, 
+//* poi leggere il file con buffer = alla lunghezza della stringa esaminata.
+comparare le due attraverso la comparazione con le struct,dopo una lettura in cui non viene trovata la stringa mandare il puntatore indietro di 3 e stampare solo il primo char; altrimenti stampare il char, inoltre se si rileva la stringa mandare il puntatore indietro di uno e se non si rileva di nuovo la stringa stampare il buffer meno il primo char.
+*/
 
-int main()
-{   
-    while (1)
-    {
-        printf("%d\n",((1<<rand()%9+0)*rand()%2+0));
+
+int main(){
+    int i=0;
+    char Buf[DIM];
+    char Str[DIM]="effe";
+
+    printf("Str (4char):");
+    scanf("%s",&Str);
+
+    printf("buf (4char):");
+    scanf("%s",&Buf);
+
+    for(i=0; i<DIM; i++){
+        printf("%c ",Buf[i]);
     }
-    
+    putchar('\n');
+    for(i=0; i<DIM; i++){
+        printf("%c ",Str[i]);
+    }
+    printf("FINE");
+    return 0;
 }
