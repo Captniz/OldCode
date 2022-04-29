b589d11c4414193673f42c4859917260dea6477d
Simone Tardiani <Simone.Tardiani@gmail.com>
2022-04-29 11:40:02 +0200

    .

--- VS/vs/C/Es_Informatica_2021_Marco/Verifiche/V5/prg2.c
+++ VS/vs/C/Es_Informatica_2021_Marco/Verifiche/V5/prg2.c
@@ -1,5 +1,17 @@
-int rico(int num, int result){
+#include<stdio.h>
+#include<stdlib.h>
+int rico(int num, int result);
 
+int main(){
+   int num,result;
+   printf("Inserisci un numero: ");
+   scanf("%d",&num);
+   printf("\n%d",rico(num,0));
+   
+   return 0;
+}
+
+int rico(int num, int result){
    if(num!=0){
       result = result*10 + num%10;
       return rico((num/10),result);
