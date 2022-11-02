679162c7ae9430207e96b6b8376e0f7abd57cc10
Simone-Tardiani <simone.tardiani@gmail.com>
2022-11-02 17:52:17 +0100

    added 2 cpp excerises

--- VS/vs/C/Es_Informatica_2021_Emanuele/test.cpp
+++ VS/vs/C/Es_Informatica_2021_Emanuele/test.cpp
@@ -1,12 +1,43 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
+class Test{
+    private:
+        string v[10]; 
+        int numElem;
+    public:
+        Test(int numElem){
+            for(int i = 0; i < numElem; i++){
+                cout << "Inserisci la stringa: ";
+                cin >> v[i];
+            }
+        }
+        void ordina(int numElem){
+            string temp;
+            for(int i = 0; i < numElem; i++){
+                for(int j = i + 1; j < numElem; j++){
+                    if(v[i] < v[j]){
+                        temp = v[i];
+                        v[i] = v[j];
+                        v[j] = temp;
+                    }
+                }
+            }
+        }
+        void stampa(int numElem){
+            for(int i = 0; i < numElem; i++){
+                cout << v[i] << " ";
+            }
+        }
+};
+
 int main(){
-    string str = "ciao";
-    string str2;
-
-    str2 = str;
-
-    cout<<str2;
+    int numElem=10;
+    cout << "Inserisci il numero di elementi: ";
+    cin >> numElem;
+    Test oggetto(numElem);
+    oggetto.ordina(numElem);
+    oggetto.stampa(numElem);
+    return 0;
 }
