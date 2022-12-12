df2de83a3e90ea69c28a5f658b3accb06f643ea7
Simone-Tardiani <simone.tardiani@gmail.com>
2022-12-12 22:05:41 +0100

    .

---
+++ VS/vs/C/221212_convertToString_TardianiSimone.cpp
@@ -0,0 +1,17 @@
+#include<string>
+#include<iostream>
+using namespace std;
+
+int main(){
+    //convert a int to string
+    int n = 1;
+    string s = "a";
+
+    char s1[100];
+    sprintf(s1,"%d",n);
+    string s2 = string(s1);
+    
+    s = s + s2;
+    cout << s <<'~'<<s1[0]<< endl;
+    return 0;
+}
